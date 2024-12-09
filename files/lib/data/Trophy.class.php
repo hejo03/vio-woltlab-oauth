a06b7451ef3cc6004b3f59b211db24151a1dd061
Joel <47361759+hejo03@users.noreply.github.com>
2024-12-09 08:40:15 +0100

    Create Trophy.class.php

---
+++ files/lib/data/Trophy.class.php
@@ -0,0 +1,8 @@
+namespace wcf\data\trophy;
+
+use wcf\data\DatabaseObject;
+
+class Trophy extends DatabaseObject {
+    protected static $databaseTableName = 'trophy';
+    protected static $databaseTableIndexName = 'trophyID';
+}
