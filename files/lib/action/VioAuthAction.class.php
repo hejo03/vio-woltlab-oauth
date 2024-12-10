bcec515f8ce4605140f6cffbcdb5ebfec54dea16
Joel <hejo03@gmx.de>
2024-12-10 21:51:19 +0100

    Update

---
+++ files/lib/action/VioAuthAction.class.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace wcf\action;
+
+use wcf\page\OAuthCallbackPage;
+use wcf\system\request\LinkHandler;
+use wcf\util\HeaderUtil;
+
+
+final class VioAuthAction extends AbstractAction
+{
+    const AVAILABLE_DURING_OFFLINE_MODE = true;
+
+    public function execute()
+    {
+        parent::execute();
+
+        session_start();
+
+        $codeVerifier = bin2hex(random_bytes(60)); // Code Verifier generieren
+        $codeChallenge = rtrim(strtr(base64_encode(hash('sha256', $codeVerifier, true)), '+/', '-_'), '='); // Code Challenge generieren
+
+        // Speichere den Code Verifier in der Session
+        $_SESSION['code_verifier'] = $codeVerifier;
+
+
+        // Erstelle den State (wie von TypeScript)
+        $state = rtrim(strtr(base64_encode(random_bytes(25)), '+/', '-_'), '=');
+
+        $_SESSION['state'] = $state;
+
+        // Definiere die OAuth-Parameter
+        $clientId = VIO_OAUTH_CLIENT_ID;
+        $redirectUri = LinkHandler::getInstance()->getControllerLink(OAuthCallbackPage::class);
+        $scopes = ['read.self'];
+        $scopeString = implode(' ', $scopes);
+
+        // Baue die OAuth-URL zusammen
+        $oauthUrl = 'https://apiv1.vio-v.com/api/oauth2/authorize?' . http_build_query([
+                'client_id' => $clientId,
+                'redirect_uri' => urldecode($redirectUri),
+                'response_type' => 'code',
+                'scope' => $scopeString,
+                'state' => $state,
+                'code_challenge' => $codeChallenge,
+                'code_challenge_method' => 'S256'
+            ]);
+
+        HeaderUtil::redirect($oauthUrl);
+    }
+
+}
--- templates/__loginVioAuth.tpl
+++ templates/__loginVioAuth.tpl
@@ -1,7 +1,7 @@
 {if VIO_OAUTH_CLIENT_ID !== '' && VIO_OAUTH_CLIENT_SECRET !== '' && VIO_OAUTH_EMAIL_PLACEHOLDER !== ''}
     <li id="vioAuthConnectAuth" class="thirdPartyLogin">
-        <a href="{link controller='OAuthLogin'}{/link}" class="button thirdPartyLoginButton vioAuthConnectLoginButton">
-            <img src="https://apiv1.vio-v.com/images/favicon.ico" alt="vioicon" style="width: 32px; height: 32px;"/>
+        <a href="{link controller='VioAuth'}{/link}" class="button thirdPartyLoginButton vioAuthConnectLoginButton">
+            <img src="https://apiv1.vio-v.com/images/favicon.ico" alt="vioicon" style="width: 24px; height: 24px;"/>
             <span>{lang}wcf.user.3rdparty.viooauth.login{/lang}</span>
         </a>
     </li>
